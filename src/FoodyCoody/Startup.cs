6787da1c5b9eda312b7f561a42650de435ab4886
Tane Yoroshi <taneyoroshi@gmail.com>
2016-08-23 23:59:26 +0800

    Added appsettings.json

--- src/FoodyCoody/Startup.cs
+++ src/FoodyCoody/Startup.cs
@@ -7,11 +7,22 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace FoodyCoody
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -30,7 +41,8 @@ namespace FoodyCoody
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greeting = Configuration["greeting"];
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
